924100b69d6d837f9778ed0a57fa521317d07909
dyllan-Lee <dltjrwls0802@gmail.com>
2023-02-06 09:12:16 +0900

    font

--- src/main.js
+++ src/main.js
@@ -12,6 +12,32 @@ import {
   createApp
 } from "vue";
 
+/* import the fontawesome core */
+import {
+  library
+} from '@fortawesome/fontawesome-svg-core'
+
+/* 
+   import specific icons 
+   원하는 아이콘을 개별적으로 import
+*/
+import {
+  faUserSecret
+} from '@fortawesome/free-solid-svg-icons'
+
+/* import font awesome 컴포넌트 */
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
+
+/* 위에서 import한 아이콘들을 Core library에 등록 */
+library.add(faUserSecret)
+
+/* font awesome 컴포넌트를 전역으로 등록 */
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+// Vue.config.productionTip = false
+
 
 export const app = createApp(App)
 app.AOS = new AOS.init({
